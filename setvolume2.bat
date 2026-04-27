7dd16003b86e9c3973344d65b90204b1426d6c95
Muhammad Tahmid Ahnaf <this.is.ahnaf101@gmail.com>
2026-04-27 21:19:01 +0600

    Add files via upload

---
+++ setvolume2.bat
@@ -0,0 +1,7 @@
+@echo off
+:: Note: Navigate and paste the path of where you kept SoundVolumeView.exe exactly 
+:: where I've wrote (Enter path). The code won't work otherwise.
+:: It should look like this example:
+:: E:\Downloaded Softwares\soundvolumeview-x64\SoundVolumeView.exe
+
+"(Enter path)" /SetVolume "AirPods Pro2\Device\Earpods\Render" 24
